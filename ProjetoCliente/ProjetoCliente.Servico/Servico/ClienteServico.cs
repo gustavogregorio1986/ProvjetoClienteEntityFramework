78b64d5191c485f2fbc7414ebb9782b696c2ba9d
guga56 <gustavogregorio086@gmail.com>
2023-11-06 15:25:42 -0300

    Desenvolvimento de serviços e interfaces com suas dependencias

---
+++ ProjetoCliente/ProjetoCliente.Servico/Servico/ClienteServico.cs
@@ -0,0 +1,31 @@
+﻿using ProjetoCliente.Data.Repositorio.Interface;
+using ProjetoCliente.Dominio.Dominio;
+using ProjetoCliente.Servico.Servico.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCliente.Servico.Servico
+{
+    public class ClienteServico : IClienteServico
+    {
+        private readonly IClienteRepositorio _clienteRepoositorio;
+
+        public ClienteServico(IClienteRepositorio clienteRepositorio)
+        {
+            _clienteRepoositorio = clienteRepositorio;
+        }
+
+        public void AdicionaarCliente(Cliente cliente)
+        {
+            _clienteRepoositorio.Adicionar(cliente);
+        }
+
+        public List<Cliente> ListarClientes()
+        {
+            return _clienteRepoositorio.ListarClientes();
+        }
+    }
+}
---
+++ ProjetoCliente/ProjetoCliente/Models/ClienteModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoCliente.Models
+{
+    public class ClienteModel
+    {
+        public int IdCliente { get; set; }
+
+        public string NomeCliente { get; set; }
+
+        public string EmailCliente { get; set; }
+
+        public string Cpf { get; set; }
+    }
+}
